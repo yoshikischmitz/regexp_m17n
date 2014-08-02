b0bb000d32136236228f83939dce967e4b4f3683
Yoshiki <yoshikischmitz@outlook.com>
2014-08-01 22:07:17 -0600

    comment on encoding

--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -4,6 +4,8 @@ require_relative '../lib/regexp_m17n'
 
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
+    # No point testing against dummy encodings. 
+    #   see: http://www.ruby-doc.org/core-2.1.2/Encoding.html#method-i-dummy-3F
     Encoding.list.reject(&:dummy?).each do |enc|
       assert(RegexpM17N.non_empty?('.'.encode(enc)))
     end
