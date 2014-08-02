e2f4bcd8e98cd4299fe659ba30e89cf7c6bee188
Yoshiki <yoshikischmitz@outlook.com>
2014-08-01 22:03:17 -0600

    encode regex to match the string, reject dummy encodings

--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -4,7 +4,7 @@ require_relative '../lib/regexp_m17n'
 
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
-    Encoding.list.each do |enc|
+    Encoding.list.reject(&:dummy?).each do |enc|
       assert(RegexpM17N.non_empty?('.'.encode(enc)))
     end
   end
