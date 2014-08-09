55373fcc8892388bf61e9ff107f157d0115392dc
Yoshiki <yoshikischmitz@outlook.com>
2014-08-08 21:18:18 -0600

    compatible with all but 2 unimplemented encodings

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,11 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ Regexp.new("^.+$".encode(str.encoding))
+    if str.encoding.dummy?
+      # Convert through pairs of compatible encodings until we reach UTF-8
+      Encoding::Converter.search_convpath(str.encoding, "UTF-8").each do |e|
+        2.times{ str.encode!(e.shift) }
+      end
+    end
+    str =~ Regexp.new('^.+$'.encode(str.encoding))
   end
 end
--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -3,10 +3,13 @@ require 'minitest/autorun'
 require_relative '../lib/regexp_m17n'
 
 class RegexpTest < MiniTest::Unit::TestCase
+  # Ruby does not implement converters for these:
+  IGNORE_LIST = ['ISO-2022-JP-2', 'UTF-7']
+
+  TEST_ENC_LIST = Encoding.list.reject{|x| IGNORE_LIST.include? x.name}
+
   def test_non_empty_string
-    # No point testing against dummy encodings. 
-    #   see: http://www.ruby-doc.org/core-2.1.2/Encoding.html#method-i-dummy-3F
-    Encoding.list.reject(&:dummy?).each do |enc|
+    TEST_ENC_LIST.each do |enc|
       assert(RegexpM17N.non_empty?('.'.encode(enc)))
     end
   end
